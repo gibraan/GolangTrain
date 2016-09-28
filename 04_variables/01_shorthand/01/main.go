c3443eeb37b675fae1b3db1b6ed5b461c690057b
gibraan <gibraan@gmail.com>
2016-09-28 20:00:37 +0100

    variables and scope training

---
+++ 04_variables/01_shorthand/01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "go lang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
---
+++ 04_variables/01_shorthand/03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	Name := "Gibran"
+	var Height int
+	Height = 187
+
+	fmt.Printf("%v \n", Name)
+	fmt.Printf("%v \n", Height)
+
+}
